4a0d4b43115973950c9bc2873e957a877c966a5f
Dylan1382 <30166840@nescol.ac.uk>
2023-08-30 15:28:43 +0100

    Hello world 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-       System.out.println("Hello World! 2023 1 ");
+
+        String[] line1 = {"Flakes fall softly down",
+                "Crystal flakes drifting"};
+
+        String[] line2 = {"Blanketing the ground in white",
+                "Tree branches wear icy gowns"};
+
+        String[] line3 = {"Winter's cold beauty",
+                "Snow covers all still"};
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(line1[i]);
+            System.out.println(line2[i]);
+            System.out.println(line3[i]);
+            System.out.println();
         }
     }
+
+}
