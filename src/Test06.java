7257f80c22e53e6b82cfbb37d7bc8bc6aef35f92
Dylan <30166840@nescol.ac.uk>
2023-09-06 14:25:15 +0100

    input name and Bobby then print

--- src/Test06.java
+++ src/Test06.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class Test06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your favorite hobby: ");
+        String hobby = scanner.nextLine();
+
+        System.out.println("Your name is: " + name);
+        System.out.println("Your favorite hobby is: " + hobby);
+
+        scanner.close();
+    }
 }
