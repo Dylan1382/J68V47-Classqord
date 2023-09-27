caae9fdd06b08ea1d4f1b3d613e98fab8e16bdec
Dylan <30166840@nescol.ac.uk>
2023-09-27 13:42:56 +0100

    asks 3 questions then you answer them and get true or false and CAPS DonT MattEr

---
+++ src/lession5.java
@@ -0,0 +1,8 @@
+public class lesson_5 {
+
+    public static void main(String[] args) {
+        for (int count = 1; count <= 12; count++) {
+            System.out.println(count);
+        }
+    }
+}
