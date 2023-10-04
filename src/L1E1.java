8a3d94092b35f3ef321b3e91f5a4a263ff50bda4
Dylan1382 <143611831+Dylan1382@users.noreply.github.com>
2023-10-04 12:58:06 +0100

    Add files via upload

---
+++ src/L1E1.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] line1 = {"Flakes fall softly down",
+                "Crystal flakes drifting"};
+
+        String[] line2 = {"Blanketing the ground in white",
+                "Tree branches wear icy gowns"};
+
+        String[] line3 = {"Winter's cold beauty",
+                "Snow covers all still"};
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(line1[i]);
+            System.out.println(line2[i]);
+            System.out.println(line3[i]);
+            System.out.println();
+        }
+    }
+
+}
