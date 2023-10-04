21caf5bb376f75a3085be14844b6e3459e6193ae
Dylan <30166840@nescol.ac.uk>
2023-10-04 13:32:11 +0100

    counts 10 to 24 in 2s

--- src/L1E1.java
+++ src/L1E1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class L1E1 {
 
     public static void main(String[] args) {
 
---
+++ src/L6E1.java
@@ -0,0 +1,12 @@
+public class L6E1 {
+    public static void main(String[] args) {
+        int number = 10; // Start from 10
+
+        while (number <= 24) { // Continue until 24
+            if (number % 2 == 0) { // Check if the number is even
+                System.out.println(number); // Print the even number
+            }
+            number++; // Increment the number by 1
+        }
+    }
+}
