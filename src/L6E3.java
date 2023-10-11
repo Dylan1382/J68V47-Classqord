bdc967742849c886010296f93ce639ebd1dd03e5
Dylan <30166840@nescol.ac.uk>
2023-10-11 10:28:44 +0100

    3 lives to 10 questions

---
+++ src/L6E3.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class L6E3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int score = 0;
+        int lives = 3;
+
+        System.out.println("Welcome to the Math Quiz Game!");
+        System.out.println("You have 3 lives. Let's get started!");
+
+        for (int i = 1; i <= 10; i++) {
+            int num1 = random.nextInt(10); // generates a random number between 0 and 9
+            int num2 = random.nextInt(10); // generates another random number between 0 and 9
+            int correctAnswer = num1 + num2;
+
+            System.out.println("Question " + i + ": What is " + num1 + " + " + num2 + "?");
+            System.out.print("Your answer: ");
+            int userAnswer = scanner.nextInt();
+
+            if (userAnswer == correctAnswer) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("Incorrect. The correct answer is " + correctAnswer);
+                lives--;
+
+                if (lives == 0) {
+                    System.out.println("Game Over! You ran out of lives.");
+                    break;
+                }
+            }
+        }
+
+        if (score == 10) {
+            System.out.println("\nWell done! You answered all questions correctly!");
+        }
+
+        System.out.println("\nQuiz completed!");
+        System.out.println("Your score: " + score + " out of 10");
+
+        scanner.close();
+    }
+}
